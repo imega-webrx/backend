e033647e29f888d60e541d1db0074c68d75c0124
tsarapoid <nadein@gmail.com>
2021-03-16 22:11:10 +0300

    add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,44 @@ module.exports = {
         ecmaVersion: 12,
     },
     plugins: ["node", "import", "promise"],
+    // disables eslint check of configuration files in the project root dir
+    // not required if rules:"sort-keys" disabled
     ignorePatterns: ["/*.*"],
     rules: {
-        indent: "off",
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double", { allowTemplateLiterals: true }],
+        semi: ["error", "always"],
+        curly: ["error"],
+        "space-before-function-paren": [
+            "error",
+            {
+                anonymous: "never",
+                named: "never",
+                asyncArrow: "always",
+            },
+        ],
+        "one-var": ["error", { var: "always", let: "never", const: "never" }],
+        // warns if elements in objects are not ordered in ascending order
+        "sort-keys": [
+            "warn",
+            "asc",
+            { caseSensitive: true, natural: false, minKeys: 2 },
+        ],
+        eqeqeq: ["error", "smart"],
+        complexity: ["warn", { max: 2 }],
+        "no-magic-numbers": [
+            "error",
+            { ignore: [-1, 0, 1], ignoreArrayIndexes: true },
+        ],
+        "no-param-reassign": ["error", { props: false }],
+        "no-unused-vars": [
+            "error",
+            {
+                args: "after-used",
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+            },
+        ],
     },
 };
